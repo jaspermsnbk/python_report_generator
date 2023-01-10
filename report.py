8e0d92121e5a1b4ff9a15f2eee42f4ad555c467c
Tedec1 <jaspermesenbrink@gmail.com>
2023-01-09 22:11:31 -0700

    added handle_file method and started build_report

--- report.py
+++ report.py
@@ -1,5 +1,8 @@
+import csv
+
+
 class transaction():
-    def __init__(self, date, description, amount, trans_type, category, account_name, labels, notes):
+    def __init__(self, date, description, amount, trans_type, category, account_name, labels="", notes=""):
         self.date = date
         self.description = description
         self.amount = amount
@@ -12,7 +15,30 @@ class transaction():
     pass
 
 
+def handle_file():
+    filename = input("transactions file to use: ")
+    data = []
+    skip = True
+    with open(filename, "r") as trans:
+        reader = csv.reader(trans)
+        for l in reader:
+            if skip:
+                skip = False
+                continue
+            data.append(
+                transaction(l[0], l[1], l[2], float(l[3]), l[4], l[5], l[6], l[7]))
+    print(f"${len(data)} transactions were read")
+    return data
+
+
 def main():
+    data = handle_file()
+    build_report(data)
+    return
+
+
+def build_report(data):
+    print(len(data))
 
     pass
 

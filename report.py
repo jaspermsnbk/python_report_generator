5d277ae0cb3cb9990ea59d6bd174ed792492ba4d
Tedec1 <jaspermesenbrink@gmail.com>
2023-01-09 23:30:56 -0700

    refactor and completed build_report method

--- report.py
+++ report.py
@@ -1,18 +1,11 @@
 import csv
+# from typing import List, Dict
 
 
-class transaction():
-    def __init__(self, date, description, amount, trans_type, category, account_name, labels="", notes=""):
-        self.date = date
-        self.description = description
-        self.amount = amount
-        self.trans_type = trans_type
-        self.category = category
-        self.account_name = account_name
-        self.labels = labels
-        self.notes = notes
-
-    pass
+def main():
+    data = handle_file()
+    build_report(data)
+    return
 
 
 def handle_file():
@@ -26,20 +19,67 @@ def handle_file():
                 skip = False
                 continue
             data.append(
-                transaction(l[0], l[1], l[2], float(l[3]), l[4], l[5], l[6], l[7]))
-    print(f"${len(data)} transactions were read")
+                Transaction(", ".join([l[0], l[3], l[5]]), l[0], l[1], l[2], float(l[3]), l[4], l[5], l[6], l[7]))
+    print(f"{len(data)} transactions were read")
     return data
 
 
-def main():
-    data = handle_file()
-    build_report(data)
-    return
-
-
-def build_report(data):
+def build_report(data: list):
     print(len(data))
 
+    months = [[], [], [], [], [], [], [], [], [], [], [], []]
+    report = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+    # loop through transactions
+    # if month is in the map then append transaction to list
+    for t in data:
+        month = int(t.date.split("/")[0]) - 1  # get month
+        months[month].append(t)  # append transaction to month
+    # i = 1
+    # for m in months: #print out raw data
+    #     print(i)
+    #     for t in m:
+    #         print(str(t))
+    #     i += 1
+    j = 0
+    for m in months:
+        cur = report[j]
+        for t in m:
+            if t.category in cur:
+                cur[t.category] += t.amount
+            else:
+                cur[t.category] = t.amount
+        j += 1
+        pass
+    # print report
+    z = 1
+    for m in report:
+        print(z)
+        for k, v in m.items():
+            print(f"{k}: {v:.2f}")
+        z += 1
+    return [report, months]
+
+
+class Transaction():
+    positive = ["Income", "Paycheck", "Investments",
+                "Transfer", "Returned Purchase", "Reimbursement"]
+
+    def __init__(self, s: str, date: str, description: str, o_description: str, amount: float, trans_type: str, category: str, account_name: str, labels="", notes=""):
+        self.date = date
+        self.description = description
+        self.o_description = o_description
+        self.amount = amount
+        self.trans_type = trans_type
+        self.category = category
+        self.account_name = account_name
+        self.labels = labels
+        self.notes = notes
+        self.s = s
+        if self.category not in self.positive:
+            self.amount *= -1
+
+    def __str__(self) -> str:
+        return self.s
     pass
 
 

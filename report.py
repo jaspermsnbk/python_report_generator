82e64094cc1a515732596051a1e5e870bb3bb06a
Jasper <jaspermesenbrink@gmail.com>
2023-01-09 21:40:40 -0700

    init commit

---
+++ report.py
@@ -0,0 +1,20 @@
+class transaction():
+    def __init__(self, date, description, amount, trans_type, category, account_name, labels, notes):
+        self.date = date
+        self.description = description
+        self.amount = amount
+        self.trans_type = trans_type
+        self.category = category
+        self.account_name = account_name
+        self.labels = labels
+        self.notes = notes
+
+    pass
+
+
+def main():
+
+    pass
+
+
+main()
